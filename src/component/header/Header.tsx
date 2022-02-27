eef06e519ca25cbf109a15059529c891f786fcdd
Shota Taniguchi <st11121.grtkmch@gmail.com>
2022-02-27 17:24:54 +0900

    install sass

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
+import styles from '../../styles/components/Header.module.scss';
 
 interface HeaderProps {}
 
 const Header: React.VFC<HeaderProps> = () => {
-    return <div className=""></div>;
+    return <div className={styles.test}>asdfasdfasdfasdfasdfas</div>;
 };
 
 export default Header;
