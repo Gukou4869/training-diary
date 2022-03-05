a1783abbf0c37dc8350e016d0cdfa5db3f1d7390
Shota Taniguchi <st11121.grtkmch@gmail.com>
2022-03-05 23:23:38 +0900

    add firebasae auth

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,16 +1,14 @@
-// firebaseをimportしています
-import firebase from 'firebase/app';
+import { initializeApp } from 'firebase/app';
 
-const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_MASSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+const config = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSEGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
-// Firebaseのインスタンスが存在しない場合にのみ、インスタンスを作成します
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-}
+
+const firebaseApp = initializeApp(config);
+export default firebaseApp;
