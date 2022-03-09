717be969b549e07bf8034ee5b5c58db00e3b89c0
Shota Taniguchi <st11121.grtkmch@gmail.com>
2022-03-10 08:11:10 +0900

    fix sign up func

--- src/services/auth/Signout.ts
+++ src/services/auth/Signout.ts
@@ -1,15 +1,13 @@
 import { signOut } from 'firebase/auth';
 import { auth } from '../../../firebase/firebase';
 
-const signout = (): void => {
-    signOut(auth)
-        .then(() => {
-            // Sign-out successful.
-            console.log('logout!!!');
-        })
-        .catch(error => {
-            // An error happened.
-        });
-};
+async function signout(): Promise<void> {
+    try {
+        await signOut(auth);
+        console.log('sign out complete!');
+    } catch (e) {
+        throw e;
+    }
+}
 
 export default signout;
--- src/services/auth/Signup.ts
+++ src/services/auth/Signup.ts
@@ -1,25 +1,18 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import {
+    createUserWithEmailAndPassword,
+    User,
+    UserCredential,
+} from 'firebase/auth';
 import { auth } from '../../../firebase/firebase';
-import { IError } from '@/store/error/models';
 
-const signup = (email: string, password: string): void => {
-    createUserWithEmailAndPassword(auth, email, password)
-        .then(userCredential => {
-            // Signed in
-            const user = userCredential.user;
-            // ...
-        })
-        .catch(error => {
-            const errorObj: IError = {
-                hasError: true,
-                errorType: error.code,
-                errorMessage: error.message,
-            };
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // ..
-            return errorObj;
-        });
-};
+async function signup(email: string, password: string): Promise<User> {
+    try {
+        const userCredential: UserCredential =
+            await createUserWithEmailAndPassword(auth, email, password);
+        return userCredential.user;
+    } catch (e) {
+        throw e;
+    }
+}
 
 export default signup;
