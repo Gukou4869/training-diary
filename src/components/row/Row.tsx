01f1a4ae45e64e954fb1ccf3da28fa722f2c51e2
Shota Taniguchi <st11121.grtkmch@gmail.com>
2022-03-15 17:54:06 +0900

    delete row

--- src/components/row/Row.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import stlyes from './Row.module.scss';
-
-const Row: React.FC = props => {
-    const { children } = props;
-    return <div className={stlyes.row}>{children}</div>;
-};
-
-export default Row;
